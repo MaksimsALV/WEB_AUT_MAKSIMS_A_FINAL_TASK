4bc1346cd40afabacb4f8b6c88827db1f3d9c9c3
MaksimsALV <maxim.ahmetov@gmail.com>
2026-03-15 13:56:29 +0200

    getState getCity

--- cypress/e2e/pageObjects/PracticeFormPage.js
+++ cypress/e2e/pageObjects/PracticeFormPage.js
@@ -9,6 +9,8 @@ export class PracticeFormPage {
         cy.get('#subjectsInput').type('Economics{enter}');
         cy.get('#hobbies-checkbox-3').check();
         cy.get('#currentAddress').type('Delhi street 123, korpuss-5, India');
+        this.getState();
+        this.getCity();
     }
 
     static calendarWidgetSelection() {
@@ -16,6 +18,16 @@ export class PracticeFormPage {
         cy.get('.react-datepicker__year-select').select('1930');
         cy.get('.react-datepicker__month-select').select('February');
         cy.get('.react-datepicker__day').contains('28').click();
+    }
+
+    static getState() {
+        cy.get('#state').click();
+        cy.contains('NCR').click();
+    }
+
+    static getCity() {
+        cy.get('#city').click();
+        cy.contains('Delhi').click();
     }
 
     static clickOnSubmitButton() {
